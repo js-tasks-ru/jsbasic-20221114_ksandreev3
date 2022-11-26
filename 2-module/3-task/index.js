3f085db51711e13dcbf3670797a4a01ab223c413
ksandreev <ksandreev3@gmail.com>
2022-11-26 20:53:23 +0300

    Module #2 completed

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
 };
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
