2482c46bef2fdff99fc7119c7a5aca6168540b70
ksandreev <ksandreev3@gmail.com>
2022-12-02 12:10:57 +0300

    Module #3 completed

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("-");
+  arr.forEach((item) => {
+    arr[arr.indexOf(item)] =
+      item.charAt(0).toUpperCase() + item.slice(1, item.length);
+  });
+  str = arr.join("");
+  return str.charAt(0).toLowerCase() + str.slice(1, str.length);
 }
+
+camelize();
