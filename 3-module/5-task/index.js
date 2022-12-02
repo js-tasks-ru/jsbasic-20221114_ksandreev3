ef12f9706b5bce411fac64925f4a18ba9182b0fb
ksandreev <ksandreev3@gmail.com>
2022-12-02 12:12:10 +0300

    module #3 completed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbersOnly = str
+    .split(",")
+    .join(" ")
+    .split(" ")
+    .filter((num) => isFinite(num));
+
+  let result = {
+    min: Math.min(...numbersOnly),
+    max: Math.max(...numbersOnly),
+  };
+  return result;
 }
