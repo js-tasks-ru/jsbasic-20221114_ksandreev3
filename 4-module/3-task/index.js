020958a3cea6ac633689e416bece9a4b1752dcd0
ksandreev <ksandreev3@gmail.com>
2022-12-05 17:59:42 +0300

    4-module completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.tBodies[0].rows) {
+    let available = row.cells[3].dataset.available;
+    if (available) {
+      row.classList.add(available === "true" ? "available" : "unavailable");
+    } else {
+      row.hidden = true;
+    }
+
+    let gender = row.cells[2].innerHTML;
+    if (gender === "m") {
+      row.classList.add("male");
+    }
+    if (gender === "f") {
+      row.classList.add("female");
+    }
+
+    let age = row.cells[1].innerHTML;
+    if (age < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
